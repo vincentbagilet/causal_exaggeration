8facc5070e1f6da63d9ad9d72b606a62221497c9
Vincent Bagilet <vincent.bagilet@columbia.edu>
2024-05-15 20:07:34 -0400

    Add lit reviews

--- functions.R
+++ functions.R
@@ -14,3 +14,23 @@ summarise_sim <- function(data, varying_params, true_effect) {
     ) |>  
     ungroup() 
 } 
+
+summary_power <- function(data, lint_names = FALSE) {
+  summary_power <- data |>
+    summarise(
+      median_exagg = median(type_m),
+      `3rd_quartile_exagg` = quantile(type_m, 0.75),
+      prop_larger_2 = mean(type_m > 2)*100,
+      median_power = median(power),
+      `3rd_quartile_power` = quantile(power, 0.75)
+    ) |> 
+    round(1)
+  
+  if (lint_names) {
+    summary_power <- summary_power |>
+      rename(`Prop Exagg Larger Than 2 (%)` = prop_larger_2) |> 
+      rename_with(\(x) str_to_title(str_replace_all(x, "_", " ")))
+  }
+  
+  return(summary_power)
+}
